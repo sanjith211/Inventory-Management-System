ab00b1327d910110d33746fec36d25bf7bad6814
sanjith211 <sanjith211@gmail.com>
2024-02-18 22:33:21 +0530

    Commit changes

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,35 +1,36 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = mongoose.Schema(
   {
     name: {
       type: String,
-      require: [true, "Please add a name"],
+      required: [true, "Please add a name"],
     },
     email: {
       type: String,
-      require: [true, "Please add a email"],
+      required: [true, "Please add a email"],
       unique: true,
       trim: true,
       match: [
-        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-        "Please enter a valid email",
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+        "Please enter a valid emaial",
       ],
     },
     password: {
       type: String,
-      require: [true, "Please add a password"],
+      required: [true, "Please add a password"],
       minLength: [6, "Password must be up to 6 characters"],
-      maxLength: [23, "Password must not be more than 23 characters"],
+      //   maxLength: [23, "Password must not be more than 23 characters"],
     },
     photo: {
       type: String,
-      require: [true, "Please add a photo"],
+      required: [true, "Please add a photo"],
       default: "https://i.ibb.co/4pDNDk1/avatar.png",
     },
     phone: {
       type: String,
-      default: "+91",
+      default: "+234",
     },
     bio: {
       type: String,
@@ -42,5 +43,18 @@ const userSchema = mongoose.Schema(
   }
 );
 
-const User = mongoose.model("user", userSchema);
+//   Encrypt password before saving to DB
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+
+  // Hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(this.password, salt);
+  this.password = hashedPassword;
+  next();
+});
+
+const User = mongoose.model("User", userSchema);
 module.exports = User;
--- backend/server.js
+++ backend/server.js
@@ -4,27 +4,41 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const userRoute = require("./routes/userRoute");
+const productRoute = require("./routes/productRoute");
+const contactRoute = require("./routes/contactRoute");
 const errorHandler = require("./middleWare/errorMiddleware");
+const cookieParser = require("cookie-parser");
+const path = require("path");
 
 const app = express();
 
-//Middlewares
+// Middlewares
 app.use(express.json());
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://pinvent-app.vercel.app"],
+    credentials: true,
+  })
+);
 
-//Routes Middleware
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+// Routes Middleware
 app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/contactus", contactRoute);
 
-//Routes
+// Routes
 app.get("/", (req, res) => {
   res.send("Home Page");
 });
 
-//Error Middleware
+// Error Middleware
 app.use(errorHandler);
-
-//connect to DB and start server
+// Connect to DB and start server
 const PORT = process.env.PORT || 5000;
 mongoose
   .connect(process.env.MONGO_URI)
@@ -33,6 +47,4 @@ mongoose
       console.log(`Server Running on port ${PORT}`);
     });
   })
-  .catch((err) => {
-    console.log(err);
-  });
+  .catch((err) => console.log(err));
