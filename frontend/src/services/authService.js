40bdfd80b74f3901f190c35b5012d9ebdad3c991
sanjith211 <sanjith211@gmail.com>
2024-02-20 00:27:37 +0530

    Done

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 import { toast } from "react-toastify";
 
-export const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
+export const BACKEND_URL =
+  "https://inventory-management-backend-p1gw.onrender.com";
 
 export const validateEmail = (email) => {
   return email.match(
