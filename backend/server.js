d58c1e22e4d4fa33588c03c2e2ead2478fbae28d
sanjith211 <sanjith211@gmail.com>
2024-02-20 18:16:04 +0530

    localhost changes done

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: [
+      "http://localhost:3000",
+      "https://inventory-management-frontend.netlify.app/",
+    ],
     credentials: true,
   })
 );
